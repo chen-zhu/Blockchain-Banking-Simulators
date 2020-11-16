296ebdf4937e6ff8fb6fd02f156fc698ec7299a2
Chen Z <patrickzhu1130@gmail.com>
2020-11-16 15:09:07 -0800

    Auto-remove old raw data files before generating.
    - Auto-remove old raw data files before generating.

--- Data/dataGenerator.py
+++ Data/dataGenerator.py
@@ -1,13 +1,28 @@
 import random
 import string
+import pathlib
+import os
 import numpy as np
 
 port_lowerRange = 50000
 port_upperRange = 50009
-log_sizePerClient = 500
+log_sizePerClient = 6400
 
 fake_clientName = []
 
+print("\n>>> Deleting old raw data files...")
+data_path = str(pathlib.Path().absolute()) + "/Raw/"
+for file_name in os.listdir(data_path): 
+	if file_name == ".DS_Store":
+		continue
+	elif os.path.exists(data_path + file_name):
+		os.remove(data_path + file_name)
+		print("Deleted raw file: " + file_name)
+
+
+print("\n[Current settings: port_lowerRange", port_lowerRange, ", port_upperRange", port_upperRange, ", log_sizePerClient", log_sizePerClient, "]\n")
+
+print(">>> Generating new raw data files...")
 for i in range(port_lowerRange, port_upperRange + 1):  
 	fake_clientName.append(''.join(random.choice(string.ascii_uppercase) for i in range(5)))
 
@@ -16,7 +31,7 @@ for f_c in fake_clientName:
 	generated_fileName = f_c + "_" + str(port_lowerRange)
 	port_lowerRange += 1
 
-	print("FileName: " + generated_fileName)
+	print("Generated raw file: " + generated_fileName)
 	f = open("Raw/" + generated_fileName, "w")
 	while log_count <= log_sizePerClient: 
 		rand_trx_val = np.random.randint(low=1, high=10)
