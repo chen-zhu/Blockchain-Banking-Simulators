d2038dced848fe5fff6eee84915d587697946796
Chen Z <patrickzhu1130@gmail.com>
2020-11-20 22:48:02 -0800

    Finish up Paxos - first version
    STILL CONTAINS TONS OF BUGS! LOGIC IS STILL BROKEN HUMMM

---
+++ Paxos/benchmarkRun.py
@@ -0,0 +1,19 @@
+import time
+import os
+import pathlib
+from pprint import pprint
+
+port_lowerRange = "50000"
+port_upperRange = "50009"
+
+data_path = str(pathlib.Path().absolute()) + "/../Data/Raw/"
+print(data_path)
+arr = os.listdir(data_path)
+
+#*****Run Client Servers *****
+for file_name in os.listdir(data_path): 
+	if file_name == ".DS_Store":
+		continue
+	print(file_name)
+	command = "osascript -e 'tell application \"Terminal\" to do script \"cd " + str(pathlib.Path().absolute()) + " && python3 ./service.py DUMMYID DYMMYNAME " + port_lowerRange + " " + port_upperRange + " " + file_name + " \"' "
+	os.system(command)
